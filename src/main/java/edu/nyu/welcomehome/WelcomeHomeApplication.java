b993fdf681af9749624ac751d9f964a2ab3239bd
Sindhujyoti Dutta <sd6201@nyu.edu>
2024-11-19 20:48:49 -0500

    Setting up the inital files of Spring

---
+++ src/main/java/edu/nyu/welcomehome/WelcomeHomeApplication.java
@@ -0,0 +1,13 @@
+package edu.nyu.welcomehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelcomeHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/nyu/welcomehome/WelcomeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nyu.welcomehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
