6564c3e1a064adfca7ef6d0220dddd76a3e7b638
Sindhujyoti Dutta <sd6201@nyu.edu>
2024-11-19 21:05:57 -0500

    Added a console output statement

--- src/main/java/edu/nyu/welcomehome/WelcomeHomeApplication.java
+++ src/main/java/edu/nyu/welcomehome/WelcomeHomeApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WelcomeHomeApplication {
-
+    
     public static void main(String[] args) {
+        System.out.println("Starting the server");
         SpringApplication.run(WelcomeHomeApplication.class, args);
     }
 
