2737d4a7e9430ea11a8e0201981d1c00f0998dae
rahulmallidi <rahulnaidum.72@gmail.com>
2024-12-08 12:16:13 -0500

    Corrected some mistakes in the manager part

--- src/main/resources/sql/supervise/get-all-orders.sql
+++
@@ -1,6 +0,0 @@
-SELECT o.*, d.deliveredStatus as currentStatus
-FROM Ordered o
-         LEFT JOIN Delivered d ON o.orderID = d.orderID
-WHERE o.orderStatus = 'IN PROGRESS'
-  AND (d.deliveredStatus = 'NOT YET DELIVERED' OR d.deliveredStatus IS NULL)
-ORDER BY o.orderDate DESC;
